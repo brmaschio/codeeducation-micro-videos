1818ae6df943e864307db803117ce59cc225f211
bhsmaschio <br.maschio@gmail.com>
2020-05-20 23:36:38 -0300

    first commit

---
+++ app/Models/Category.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
+
+class Category extends Model
+{
+
+    use SoftDeletes;
+    protected $fillable = ['name', 'description', 'is_active'];
+    protected $dates = ['deleted_at'];
+
+    protected $casts = [
+        'id' => 'string',
+        'is_active' => 'boolean',
+    ];
+
+    public static function boot()
+    {
+        parent::boot();
+        
+        static::creating(function($obj){
+            $obj->id = Uuid::uuid4();
+        });
+    
+    }
+}
