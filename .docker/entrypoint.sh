877b2b9a3ffb66f598d68706546053cc6e0bf975
bhsmaschio <br.maschio@gmail.com>
2020-06-19 10:13:27 -0300

    att env files

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
-cp .env.example .env
-cp .env.test.example .env.testing
+# cp .env.example .env
+# cp .env.test.example .env.test
 composer install
 php artisan key:generate
 php artisan migrate
--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-06-18 03:28:20.
+    * Generated on 2020-06-19 13:03:14.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
