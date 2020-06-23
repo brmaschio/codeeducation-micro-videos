34c87550ffded130a98eab1c02b437d3ed442d9c
bhsmaschio <br.maschio@gmail.com>
2020-06-23 10:49:46 -0300

    start frontend

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+npm config set cache /var/www/.npm-cache --global
+cd /var/www/frontend && npm install && cd ..
+
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 cp .docker/app/.env.test ./backend/.env.test
 cp .docker/app/.env ./backend/.env
